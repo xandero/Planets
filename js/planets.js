8bdc69120a16b674ad27450ed6dacb32c3ce1229
xandero <alexander.ohan@gmail.com>
2015-06-11 21:00:15 -0700

    further json changes

--- js/planets.js
+++ js/planets.js
@@ -1,12 +1,10 @@
 var ephemeris;
 $(document).ready(function () {
   ephemeris = $.getJSON('https://api.myjson.com/bins/2xnss');
-      debugger;
   var planet = ephemeris.responseJSON[0].name;
   var orbitCount = ephemeris.responseJSON[0].orbit.length;
-
   for (var i = 0; i < orbitCount; i++) {
-    sum += prefix[0].orbit[i];
-    
+    sum += ephemeris.responseJSON[0].orbit[i][2];
+    debugger;
   };
 });
